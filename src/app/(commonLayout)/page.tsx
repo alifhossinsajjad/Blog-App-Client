53252672d58606f3a96fba86922cbaab001e128d
alifhossinsajjad <alifhossinsajjad123456@gmail.com>
2026-09-01 12:34:15 +0600

    fix the issues

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 
 
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await authClient.getSession()
+
+console.log(session);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       {/* <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
